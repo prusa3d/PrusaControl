ad90bbaaa92c8939e9aa76d22627ed3f927ec151
Tibor Vavra <vavra.tibor@gmail.com>
2016-03-07 11:27:52 +0100

    opraveno, byla tam chyba s tim ze tam chybel tichy rezim

--- make_win.bat
+++ make_win.bat
@@ -1,2 +1,2 @@
-DEL c:\projects\slave\Windows_All\build\build\ c:\projects\slave\Windows_All\build\dist\
+DEL /F /Q c:\projects\slave\Windows_All\build\build\ c:\projects\slave\Windows_All\build\dist\
 pyinstaller --windowed --name=PrusaControll c:\projects\slave\Windows_All\build\main.py
