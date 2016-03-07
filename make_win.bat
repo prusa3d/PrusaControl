878dab3d02e87999dd238236d39afb5840ec7cb5
Tibor Vavra <vavra.tibor@gmail.com>
2016-03-07 11:47:54 +0100

    vratil jsem tam puvodni pyinstaller a jeste doplnil parametr -y pro tichy mod

--- make_win.bat
+++ make_win.bat
@@ -1,1 +1,2 @@
 DEL /F /Q c:\projects\slave\Windows_All\build\build\ c:\projects\slave\Windows_All\build\dist\
+pyinstaller -y --windowed --name=PrusaControll c:\projects\slave\Windows_All\build\main.py
