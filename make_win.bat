e3092aac78fa831abf22cf9e21cc0744863e40ba
Tibor Vavra <vavra.tibor@gmail.com>
2016-03-07 11:31:44 +0100

    pokus

--- make_win.bat
+++ make_win.bat
@@ -1,2 +1,1 @@
 DEL /F /Q c:\projects\slave\Windows_All\build\build\ c:\projects\slave\Windows_All\build\dist\
-pyinstaller --windowed --name=PrusaControll c:\projects\slave\Windows_All\build\main.py
