f8b3725e1c31ca9da3a02d00e5acfdcecc24f54d
Tibor Vavra <vavra.tibor@gmail.com>
2016-03-31 10:13:23 +0200

    doplneny zmeny v main_osx tak aby ten soubor byl totozny s main

--- main_osx.py
+++ main_osx.py
@@ -1,18 +1,32 @@
 #!/opt/local/bin/python2.7
 # -*- coding: utf-8 -*-
+from PyQt4 import QtGui
 
 from controller import Controller
 from sceneRender import *
 from sceneData import *
+import logging
+import profile
+
 
 __author__ = 'Tibor Vavra'
 
+DEBUG = False
 
-if __name__ == '__main__':
-    logging.basicConfig(filename='info.log', filemode='r', level=logging.INFO)
+def main():
     logging.info('PrusaControll start')
     app = QtGui.QApplication(sys.argv)
     controller = Controller()
     window = controller.getView()
     app.exec_()
     logging.info('PrusaControll exit')
+
+
+if __name__ == '__main__':
+    FORMAT = "[%(levelname)s][%(filename)s:%(lineno)s:%(funcName)s()]-%(message)s"
+    logging.basicConfig(format=FORMAT, filename='info.log', filemode='w', level=logging.DEBUG)
+
+    if DEBUG:
+        profile.run('main()')
+    else:
+        main()
