9aa2c655b519e3ebe7ee5ec3fc9b553f5393c3c6
Tibor Vávra <Tibor Vvra>
2016-10-20 16:00:31 +0200

    log cesty

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main():
     app = QtGui.QApplication(sys.argv)
     app.setWindowIcon(QtGui.QIcon("data/icon/favicon.ico"))
     local_path = os.path.realpath(__file__)
+    print("Local_path: " + local_path)
 
     controller = Controller(app, local_path)
     window = controller.get_view()
