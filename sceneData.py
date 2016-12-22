927dbc78233e4d7c5df8d083a391020c0508fd97
Tibor Vávra <vavra.tibor@gmail.com>
2016-12-22 17:27:53 +0100

    oprava chyby s importem

--- sceneData.py
+++ sceneData.py
@@ -9,7 +9,7 @@ from os.path import basename
 import time
 from PyQt4.QtCore import QObject
 from PyQt4.QtOpenGL import QGLBuffer
-from stl import Mode
+#from stl import Mode
 from stl.mesh import Mesh
 
 from random import randint
